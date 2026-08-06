4ae241715c16fd62046ef1e51001936486789cc9
Daniel Secco <daniel.secco@computer.org>
2026-08-06 06:52:06 -0300

    ♻️ Removal of references without purpose

--- src/decode.rs
+++
@@ -1,7 +0,0 @@
-//! Decoder: CAFE decoding functions
-//!
-//! The implementations are in cafe.rs due to dependency complexity reasons.
-//! This module only re-exports.
-
-// The public decode() function is defined in cafe.rs
-// and is re-exported in lib.rs via pub use
