1425c0b06253c26689939a0ffcf600d076eebbac
Daniel Secco <daniel.secco@computer.org>
2026-08-06 06:52:20 -0300

    ♻️ Removal of references without purpose

--- src/encode.rs
+++
@@ -1,7 +0,0 @@
-//! Encoder: CAFE encoding functions
-//!
-//! Implementations are in cafe.rs due to dependency complexity reasons.
-//! This module just re-exports.
-
-// Public functions are defined in cafe.rs
-// and are re-exported in lib.rs via pub use
